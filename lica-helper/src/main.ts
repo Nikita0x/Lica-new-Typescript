50c2a0b00581ab1971ff2c0b33ba0c8adc0c14a5
Nikita0x <klirmio21@gmail.com>
2023-05-27 01:42:19 +0300

    0.01

--- lica-helper/src/main.ts
+++ lica-helper/src/main.ts
@@ -1,16 +1,33 @@
 import './main.css'
 //Insert your HTML code here to develop offline
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-// <div class="menu-sidebar sidebar">
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<div class="menu-sidebar sidebar">
 
 
-//     <div class="container">
-//         <h1>hello</h1>
-//     </div>
 
 
-// </div>
-// `
+<div class="container">
+    <nav class="header">
+        <h2 class="header__title">Choose the language</h2>
+        <div class="header__buttons">
+            <button>Add new language</button>
+            <button>SPAM</button>
+        </div>
+    </nav>
+    <section class="body"></section>
+    <footer class="footer">
+        <div class="footer__button">
+            <h1 class="footer__title">Empty template</h1>
+            <button class="settings">Settings</button>
+        </div>
+    </footer>
+</div>
+
+
+
+
+</div>
+`
 // =======================================================================
 setTimeout(() => {
   const parent = document.querySelector('.menu-sidebar.sidebar');
